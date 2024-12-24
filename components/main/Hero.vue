703ae3661c984c4339084552c0a178f56bcf4dc5
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-12-24 21:42:03 +0530

    Responsives - / page
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- components/main/Hero.vue
+++ components/main/Hero.vue
@@ -1,38 +1,41 @@
 <template>
-    <div
-        style="width: 100vw; height: 360px; background-color: var(--primary-bg-color); padding: 10px 40px 30px 130px; position: relative;">
-        <v-col>
-            <v-row justify="space-between" style="margin: 0;">
-                <img src="/logo/theananta-logo.png" alt="theananta logo" width="70.px" height="36px">
-                <v-btn variant="tonal" style="background-color: var(--primary-color); border-radius: 100px; color: var(--quadnary-color);">
+    <div class="hero-container"
+        style="width: 100vw; background-color: var(--primary-bg-color); position: relative; overflow: hidden;">
+        <v-col class="content-container">
+            <v-row justify="end" style="margin: 0;">
+                <v-btn class="signinBtn" variant="tonal"
+                    style="background-color: var(--primary-color); border-radius: 100px; color: var(--quadnary-color);">
                     Sign in
                 </v-btn>
             </v-row>
 
-            <img src="/img/hero/hero-img.png" alt="Hero Img" style="position: absolute; bottom: 0; right: 0; z-index: 10;"
-                width="410px" height="295px">
+            <img class="heroImg" src="/img/hero/hero-img.png" alt="Hero Img"
+                style="position: absolute; bottom: 0; right: 0; z-index: 10;">
 
-            <div
+            <div class="content"
                 style="display: flex; flex-direction: column; justify-content: start; align-items: start; gap: 12px; color: white;">
-                <p style="font-size: 38px; font-weight:bolder; margin-top: 20px;">
+                <img src="/logo/theananta-logo.png" alt="theananta logo" width="70.px" height="36px">
+                <p style="font-size: 38px; font-weight:bolder;">
                     Events <br>Simplified
                 </p>
                 <p style="font-size: 12px; font-weight:400; margin-bottom: 20px;">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Natus ad, nihil iste quo maiores.
                 </p>
 
-                <v-row style="gap: 20px;">
-                    <v-btn variant="tonal" style="background-color: var(--quadnary-color); border-radius: 100px; color: var(--secondary-color);">
+                <div style="display: flex; justify-content: start; align-items: start; gap: 20px; flex-wrap: nowrap;">
+                    <v-btn variant="tonal"
+                        style="background-color: var(--quadnary-color); border-radius: 100px; color: var(--secondary-color);">
                         Host an event
                     </v-btn>
-                    <v-btn variant="outlined" style="border-color: #938F99; border-radius: 100px; color: var(--quadnary-color );">
+                    <v-btn variant="outlined"
+                        style="border-color: #938F99; border-radius: 100px; color: var(--quadnary-color );">
                         Learn more
                     </v-btn>
-                </v-row>
+                </div>
             </div>
         </v-col>
-        <div
-            style="width: 110vw; height: 50px; background-color: var(--tertiary-color); padding: 0px 10px; display: flex; justify-content: center; align-items: center; position: absolute; bottom: 0; left: 30px; rotate: -13deg;">
+        <div class="common-multer"
+            style="width: 127vw; height: 50px; background-color: var(--tertiary-color); padding: 0px 10px; display: flex; justify-content: center; align-items: center; position: absolute; left: -30px; rotate: -13deg;">
             <p style="color: white; font-size: 16px; font-weight: 500; text-wrap: nowrap;">
                 ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
                 • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
@@ -40,14 +43,100 @@
                 • ABCDE • ABCDE • ABCDE
             </p>
         </div>
+        <div class="mobile-multer"
+            style="width: 100vw; height: 50px; background-color: var(--primary-color); padding: 0px 10px; justify-content: center; align-items: center; z-index: 10;">
+            <p
+                style="color: white; font-size: 16px; font-weight: 500; text-wrap: nowrap; width: 100%; overflow: hidden;">
+                ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
+                • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
+                • ABCDE • ABCDE
+                • ABCDE • ABCDE • ABCDE
+            </p>
+        </div>
+        <div class="pc-multer"
+            style="width: 100vw; height: 50px; background-color: var(--primary-color); padding: 0px 10px; justify-content: center; align-items: center; z-index: 10; position: relative;">
+            <p
+                style="color: white; font-size: 16px; font-weight: 500; text-wrap: nowrap; width: 100%; overflow: hidden;">
+                ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
+                • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
+                • ABCDE • ABCDE
+                • ABCDE • ABCDE • ABCDE
+            </p>
+        </div>
     </div>
-    <div
-        style="width: 100vw; height: 50px; background-color: var(--primary-color); padding: 0px 10px; display: flex; justify-content: center; align-items: center; z-index: 10; position: relative;">
-        <p style="color: white; font-size: 16px; font-weight: 500; text-wrap: nowrap;">
-            ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
-            • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE • ABCDE
-            • ABCDE • ABCDE
-            • ABCDE • ABCDE • ABCDE
-        </p>
-    </div>
+
 </template>
+
+<style scoped>
+.hero-container {
+    height: 400px;
+    padding: 10px 40px 30px 130px;
+}
+
+.signinBtn {
+    display: block;
+}
+
+.heroImg {
+    width: 410px;
+    height: 295px;
+}
+
+.mobile-multer {
+    display: none;
+}
+
+.pc-multer {
+    display: flex;
+}
+
+.common-multer {
+    bottom: 0;
+}
+
+@media screen and (max-width: 1230px) {
+    .hero-container {
+        padding: 10px 20px 30px 0px;
+    }
+
+    .content-container {
+        padding-left: 20px;
+    }
+}
+
+@media screen and (max-width: 640px) {
+    .signinBtn {
+        display: none;
+    }
+
+    .content {
+        margin-top: 50px;
+    }
+
+    .hero-container {
+        height: 100vh;
+    }
+
+
+    .heroImg {
+        width: 328px;
+        height: 236px;
+        margin-bottom: 40px;
+    }
+
+    .mobile-multer {
+        display: flex;
+        position: absolute;
+        bottom: 0;
+    }
+
+    .pc-multer {
+        display: none;
+    }
+
+    .common-multer {
+        bottom: 64px;
+    }
+
+}
+</style>
